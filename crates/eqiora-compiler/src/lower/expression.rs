ad2f8e9b4d73342b7e326a4256f99458dd407e4b
Naoki Kiyohara <n.kiyohara23@imperial.ac.uk>
2026-09-07 23:14:56 +0100

    Separate checked Port contracts, identity staging, and physical accessors
    Signed-off-by: Naoki Kiyohara <n.kiyohara23@imperial.ac.uk>
    (cherry picked from commit 1d4caaa94da06417df4b97331845cb43113daa5f)
    Signed-off-by: Naoki Kiyohara <n.kiyohara23@imperial.ac.uk>

--- crates/eqiora-compiler/src/lower/expression.rs
+++ crates/eqiora-compiler/src/lower/expression.rs
@@ -1,3 +1,4 @@
+mod physical_accessors;
 use super::*;
 
 use eqiora_schema::kernel::typing::{self, ExpressionType, SpatialSupport};
@@ -852,75 +853,6 @@ impl ExpressionLowerer<'_> {
         })?;
         self.builder
             .pure_operator(definition, arguments.iter().map(|argument| argument.id))
-            .map(|id| TypedExpression { id, dimension })
-            .map_err(|diagnostic| self.builder_error(expression, diagnostic))
-    }
-
-    fn lower_physical_accessor(
-        &mut self,
-        expression: &LoweringExpression,
-        callee: &str,
-        argument: &LoweringExpression,
-    ) -> Result<TypedExpression, Diagnostic> {
-        let LoweringExpressionNode::Name(name) = argument.node.as_ref() else {
-            return Err(source_error(
-                codes::LANGUAGE_TYPE_ERROR,
-                self.file,
-                argument.range(),
-                if matches!(callee, "across" | "through") {
-                    format!("`{callee}(...)` requires one bare scalar physical Port name")
-                } else {
-                    format!("`{callee}(...)` requires one bare field-physical Port name")
-                },
-            ));
-        };
-        let Some(binding) = self.bindings.get(name) else {
-            return Err(unresolved(
-                self.file,
-                argument.range(),
-                name,
-                "scalar physical Port",
-            ));
-        };
-        let Binding::Port(port, contract) = binding else {
-            return Err(source_error(
-                codes::LANGUAGE_TYPE_ERROR,
-                self.file,
-                argument.range(),
-                format!("`{name}` is not a scalar physical Port"),
-            ));
-        };
-        let contract = resolve_port_contract(self.file, argument.range(), contract, self.bindings)?;
-        self.dependencies.insert(port.erase());
-        self.ports.insert(port.erase());
-        let (symbol, dimension) = match (callee, contract) {
-            ("across", ResolvedPortContract::ScalarPhysical { across_type, .. }) => {
-                (SymbolRef::Across(*port), across_type.dimension())
-            }
-            ("through", ResolvedPortContract::ScalarPhysical { through_type, .. }) => {
-                (SymbolRef::Through(*port), through_type.dimension())
-            }
-            ("trace", ResolvedPortContract::BoundaryPhysical { trace_type, .. }) => {
-                (SymbolRef::PortTrace(*port), trace_type.dimension())
-            }
-            ("flux", ResolvedPortContract::BoundaryPhysical { flux_type, .. }) => {
-                (SymbolRef::PortFlux(*port), flux_type.dimension())
-            }
-            _ => {
-                return Err(source_error(
-                    codes::LANGUAGE_TYPE_ERROR,
-                    self.file,
-                    argument.range(),
-                    if matches!(callee, "across" | "through") {
-                        format!("`{name}` is not a scalar physical Port")
-                    } else {
-                        format!("`{name}` is not a field-physical Port")
-                    },
-                ));
-            }
-        };
-        self.builder
-            .symbol(symbol)
             .map(|id| TypedExpression { id, dimension })
             .map_err(|diagnostic| self.builder_error(expression, diagnostic))
     }
